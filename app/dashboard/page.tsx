8f1b1a96c9473b491a5f9fe8262456b86ea3d8aa
leopestille <leonardopestille@gmail.com>
2024-11-13 18:03:31 -0400

    bug:correcao

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import { Card } from "../ui/dashboard/cards";
 import RevenueChart from "../ui/dashboard/revenue-chart";
 import LatestInvoices from "../ui/dashboard/latest-invoices";
 import { lusitana } from "../ui/fonts";
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from "../lib/data";
+import { fetchCardData, fetchLatestInvoices, fetchRevenue } from "../lib/data";
 
 export default async function Page() {
   const revenue = await fetchRevenue();
